537f767f69101b45a07baada6244a961c8992bcc
MARCO ANTONIO GUIDORIZZI COUTINHO DE SOUZA <62554313+MarcoGuidorizzi@users.noreply.github.com>
2026-04-06 15:13:47 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1C#.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            double largura, comprimento, area, precoMetroQuadrado, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = comprimento * area;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PREÇO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
